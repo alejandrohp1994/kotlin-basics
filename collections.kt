0f3c10615142499617e370e3b41f3281dbd57dc6
alejandrohp1994 <73549282+alejandrohp1994@users.noreply.github.com>
2023-06-09 18:53:48 -0400

    Update collections.kt

--- collections.kt
+++ collections.kt
@@ -1,6 +1,5 @@
 fun main() {
     //LISTS
-    /*
     
     val numbers = listOf(0,3,8,4,0,5,8,9,2)
     println("list: ${numbers}")
@@ -17,10 +16,9 @@ fun main() {
     println("$set1 == $set2: ${set1 == set2}")
     println("contains 7: ${setOfNumbers.contains(7)}")
     
-    */
 
     //MAPS
-    /*
+
     val peopleAges = mutableMapOf<String, Int>(
         "Fred" to 30,
         "Ann" to 23
@@ -41,10 +39,10 @@ fun main() {
     val filtered = peopleAges.filter {it.key.length < 4}
     // it referes at the length of the string used as a key
     println(filtered)
-     */
+
 
      //LAMBDA FUNCTIONS
-     /*
+
      //PART 1
      val triple: (Int) -> Int = {
         a: Int -> a * 3
@@ -67,7 +65,6 @@ fun main() {
     //      you can name it _ to make your code more readable and less cluttered.
     //      This code has the same behavior.
     //costOfServiceEditText.setOnKeyListener { view, keyCode, _ -> handleKeyEvent(view, keyCode) }
-     */
 
     val words = listOf("about", "acute", "awesome", "balloon", "best", "brief", "class", "coffee", "creative")
     val filteredWords = words.filter {
